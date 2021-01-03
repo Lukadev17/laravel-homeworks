147f03d0c2818e450bc576e54b6b51a7265f6c64
Luka Gagnidze <luka.gagnidze.1@btu.edu.ge>
2021-01-03 23:43:27 +0400

    Davaleba N4

---
+++ davaleba4/app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:55',
+            'email' => 'email|required|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $validatedData['password'] = bcrypt($request->password);
+
+        $user = User::create($validatedData);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response([ 'user' => $user, 'access_token' => $accessToken]);
+    }
+
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($loginData)) {
+            return response(['message' => 'Invalid Credentials']);
+        }
+
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+
+    }
+
+//    public function user_info()
+//    {
+//        /**Retrieve the information of the authenticated user
+//         */
+//        $user = Auth::user();
+//        /** Return user's details
+//         */
+//        return response()->json(['success' => $user], 200);
+//    }
+}
